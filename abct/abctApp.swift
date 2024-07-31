efae0a411bebff883eb213330595b6e283e1b408
Oluwatomiwa <tomoyee@yahoo.com>
2024-07-31 21:01:22 +0100

    Initial Commit

---
+++ abct/abctApp.swift
@@ -0,0 +1,17 @@
+//
+//  abctApp.swift
+//  abct
+//
+//  Created by User on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct abctApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
